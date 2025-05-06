c52173e10f4ba3e4d5c8e604600f0fcfdb056b66
Kirill Morozov <mokinotavailable@gmail.com>
2025-05-06 20:20:54 +0000

    Minor Syntax Fixes

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ const ANOMALY_REGEX_EXPRESSION: &str = "[^0-9*+-/()]+";
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() >= 2 {
-        let anomaly_regex: Regex = Regex::new(ANOMALY_REGEX_EXPRESSION).unwrap();
+        let anomaly_regex: Regex = Regex::new(ANOMALY_REGEX_EXPRESSION)
+            .expect(format!("Anomaly regex expression parsing error: {}", ANOMALY_REGEX_EXPRESSION).as_str());
         let expression = args[1].clone().replace(" ", "");
         if anomaly_regex.is_match(&expression) {
             println!("An anomaly was found in the expression: {}", &expression)
