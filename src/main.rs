3e164290bf5dc75aa1438dea17e150c75896705c
Kirill Morozov <mokinotavailable@gmail.com>
2025-05-06 16:22:52 +0000

    Clear and check expression

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use regex::Regex;
+
+const ANOMALY_REGEX_EXPRESSION: &str = "[^0-9*+-/()]+";
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() >= 2 {
+        let anomaly_regex: Regex = Regex::new(ANOMALY_REGEX_EXPRESSION).unwrap();
+        let expression = args[1].clone().replace(" ", "");
+        if anomaly_regex.is_match(&expression) {
+            println!("An anomaly was found in the expression: {}", &expression)
+        }
+        // calculate
+    }
+}
